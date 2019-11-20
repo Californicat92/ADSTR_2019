e054b5879721c5b237a654d9640dc1c1763a3f4c
adriGlen7 <1393277@campus.euss.org>
2019-11-20 20:24:15 +0100

    Prueba guardado

--- src/captura/libPruebas/Funciones.c
+++ src/captura/libPruebas/Funciones.c
@@ -1,88 +1,54 @@
 /*
-  * Program: Basedades
-  * 
-  * Version: 1.0
-  * 
-  * Author: Joan Olivé,Estefan Molina,Ruben Uceda
-  * 
-  * Copyright (C) 2019 
-  * 
-  * EUSS 2019-Alumnes_EUSS, GRUP B1
-  * 
-  * License GNU/GPL, see COPYING
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version.
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  * GNU General Public License for more details.
-  * You should have received a copy of the GNU General Public License
-  * along with this program. If not, see http://www.gnu.org/licenses/.
-*/
+ * Funciones.c
+ * 
+ * Copyright 2019  <pi@raspberrypi>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sqlite3.h> 
-#include <stdint.h>
-#include <unistd.h>
-#include <getopt.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <linux/types.h>
-#include <linux/spi/spidev.h>
-#include <string.h>
-#include <time.h>
-#include <getopt.h>
+//Libreria con las demás librerias, declaraciones de variables y funciones
+#include "funcADSTR.h"
 
 int verbose = 0;
 
 static char *cntdevice = "/dev/spidev0.0";// definicio pin select (0.1)/CS1
 
-//ADC configurations segons manual MCP3008
-#define SINGLE_ENDED_CH0 8
-#define SINGLE_ENDED_CH1 9
-#define SINGLE_ENDED_CH2 10
-#define SINGLE_ENDED_CH3 11
-#define SINGLE_ENDED_CH4 12
-#define SINGLE_ENDED_CH5 13
-#define SINGLE_ENDED_CH6 14
-#define SINGLE_ENDED_CH7 15
-#define DIFERENTIAL_CH0_CH1 0 //Chanel CH0 = IN+ CH1 = IN-
-#define DIFERENTIAL_CH1_CH0 1 //Chanel CH0 = IN- CH1 = IN+
-#define DIFERENTIAL_CH2_CH3 2 //Chanel CH2 = IN+ CH3 = IN-
-#define DIFERENTIAL_CH3_CH2 3 //Chanel CH2 = IN- CH3 = IN+
-#define DIFERENTIAL_CH4_CH5 4 //Chanel CH4 = IN+ CH5 = IN-
-#define DIFERENTIAL_CH5_CH4 5 //Chanel CH4 = IN- CH5 = IN+
-#define DIFERENTIAL_CH6_CH7 6 //Chanel CH6 = IN+ CH7 = IN-
-#define DIFERENTIAL_CH7_CH6 7 //Chanel CH6 = IN- CH7 = IN+
-
-// -----------------------------GETOPT HELP------------------------------------------------------------------
+// -----------------------------GETOPT HELP-------------------------------------
 void print_usage() {
-    printf(	"\n/---------------------HELP------------------------------------------\n"\
+    printf(	"\n/---------------------HELP-------------------------------\n"\
 			"Uso:	Introducir -p seguido de la IP del servidor \n" \
-			"	Introducir -r seguido de la ruta y nombre.db de la base de datos \n"\
-			"/-------------------------------------------------------------------\n\n");
+			"\tIntroducir -r seguido de la ruta y nombre.db de la base de "\
+			"datos\n"\
+			"/-----------------------------------------------------------\n\n");
 }
-// -----------------------------------------------------------------------------------------------
-
-// -----------------------------------------------------------------------------------------------
-
-static void pabort(const char *s)// Indicacion de un error
-{
+// -----------------------------------------------------------------------------
+// Indicacion de un error
+static void pabort(const char *s){
 	perror(s); // informa del error
 	abort();// cierre del programa
 }
+// -----------------------------------------------------------------------------
 
-// -----------------------------------------------------------------------------------------------
-
-static void spiadc_config_tx(int conf, uint8_t tx[3])
-{
+static void spiadc_config_tx(int conf, uint8_t tx[3]){
 	int i;
 
-	uint8_t tx_dac[3] = { 0x01, 0x00, 0x00 };// las dades de que volem trasnmetre
-	uint8_t n_tx_dac = 3;// quantitas de dades que volem transmetre 
+	uint8_t tx_dac[3] = { 0x01, 0x00, 0x00 };//Les dades de que volem trasnmetre
+	uint8_t n_tx_dac = 3;// quantitas de dades que volem transmetre
 	
 	for (i=0; i < n_tx_dac; i++) {
 		tx[i] = tx_dac[i];
@@ -91,16 +57,15 @@ static void spiadc_config_tx(int conf, uint8_t tx[3])
 // Estableix el mode de comunicació en la parta alta del 2n byte
 	tx[1]=conf<<4;
 	
-	if( verbose ) { 									//Mostra el que s'esta enviant 
+	if(verbose) { //Mostra el que s'esta enviant 
 		for (i=0; i < n_tx_dac; i++) {
 			printf("spi tx dac byte:(%02d)=0x%02x\n",i,tx[i] );
 		}
-	}
-		
+	}		
 }
+// -----------------------------------------------------------------------------
 
-// -----------------------------------------------------------------------------------------------
-static int spiadc_transfer(int fd, uint8_t bits, uint32_t speed, uint16_t delay, uint8_t tx[3], uint8_t *rx, int len){
+static int spiadc_transfer(int fd, uint8_t bits, uint32_t speed, uint16_t delay, uint8_t tx[3], uint8_t *rx, int len ){
 	int ret, value, i;
 
 	struct spi_ioc_transfer tr = {
@@ -120,17 +85,12 @@ static int spiadc_transfer(int fd, uint8_t bits, uint32_t speed, uint16_t delay,
 			printf("0x%02x ", rx[i]);
 		}
 		value = ((rx[1] & 0x0F) << 8) + rx[2];
-		printf("-->  %d\n", value);
-	
+		printf("-->  %d\n", value);	
 	}
 
 	return ret;
-
 }
-
-
-
-// -----------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 static int spiadc_config_transfer(int conf, int *value){
 	int ret = 0;
@@ -197,7 +157,7 @@ static int spiadc_config_transfer(int conf, int *value){
 
 	return ret;
 }
-
+// -----------------------------------------------------------------------------
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
    int i;
@@ -207,6 +167,7 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName){
    printf("\n");
    return 0;
 }
+// -----------------------------------------------------------------------------
 
 int openDB(char * name, sqlite3** db){ //Crear o obrir la base de dades.
 	int rc;
@@ -222,6 +183,7 @@ int openDB(char * name, sqlite3** db){ //Crear o obrir la base de dades.
 	}
 	return 0;
 }
+// -----------------------------------------------------------------------------
 
 int CreateTable(sqlite3* db){ 
 	int rc;
@@ -229,8 +191,7 @@ int CreateTable(sqlite3* db){
 	char sql1[500];
 	char *zErrMsg = 0;
 	char *sqlx;
-	
-	
+
 	    /* Check crear tabla nueva o NO*/
 	sprintf(sql1, "SELECT MAX(ID) FROM Lectures_table");	
 	rc = sqlite3_exec(db, sql1, callback, 0, &zErrMsg);
@@ -259,6 +220,7 @@ int CreateTable(sqlite3* db){
 		}
 	return 0;
 }
+// -----------------------------------------------------------------------------
 
 int CreateTable1(sqlite3* db){ 
 	int rc;
@@ -293,6 +255,7 @@ int CreateTable1(sqlite3* db){
 		}
 	return 0;
 }
+// -----------------------------------------------------------------------------
 
 int CreateTable2(sqlite3* db){ 
 	int rc;
@@ -326,8 +289,9 @@ int CreateTable2(sqlite3* db){
 		}   
 	return 0;
 }
-
-int insertTable(sqlite3* db, char* date, float value){ //Insertar en la tabla lecture
+// -----------------------------------------------------------------------------
+//Insertar en la tabla lecture
+int insertTable(sqlite3* db, char* date, float value){
 	int rc;
 	char sql[500];
 	char *zErrMsg = 0;
@@ -350,8 +314,9 @@ int insertTable(sqlite3* db, char* date, float value){ //Insertar en la tabla le
 			
 	return 0;
 }
-
-int insertTable1(sqlite3* db, char* date, float value){ //Insertar en tabla sensor
+// -----------------------------------------------------------------------------
+//Insertar en tabla sensor
+int insertTable1(sqlite3* db, char* date, float value){
 	int rc;
 	char sql[500];
 	char *zErrMsg = 0;
@@ -365,19 +330,18 @@ int insertTable1(sqlite3* db, char* date, float value){ //Insertar en tabla sens
 	sprintf(sql,"INSERT INTO Sensors_table (Types,Description) VALUES ('%s','%s');",Types, Description);
 	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
 
-		if( rc != SQLITE_OK ){
-			fprintf(stderr, "SQL error: %s\n", zErrMsg);
-			sqlite3_free(zErrMsg);
+	if( rc != SQLITE_OK ){
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
 
-			return 1;
+		return 1;
 			
 		}
-
-	return 0;
-	   
+	return 0;	   
 }
-
-int insertTable2(sqlite3* db, char* date_alarm, char* Alarm_description){ //Insertar los valores en Alarms_table
+// -----------------------------------------------------------------------------
+//Insertar los valores en Alarms_table
+int insertTable2(sqlite3* db, char* date_alarm, char* Alarm_description){
 	int rc;
 	char sql[500];
 	char *zErrMsg = 0;
@@ -393,16 +357,16 @@ int insertTable2(sqlite3* db, char* date_alarm, char* Alarm_description){ //Inse
 		sqlite3_free(zErrMsg);
 
 		return 1;
-		}
-			else{
-				fprintf(stdout, "Insercio correcta");
-			}
-	
-	return 0;
-	   
+	}
+	else{
+		fprintf(stdout, "Insercio correcta");
+	}
+	return 0; 
 }
+// -----------------------------------------------------------------------------
 
-int showTable(sqlite3* db){ //Mostrat por pantalla el contenido de la base de datos.
+//Mostrat por pantalla el contenido de la base de datos.
+int showTable(sqlite3* db){ 
 	
 	int rc;
 	char sql[500];
@@ -424,83 +388,6 @@ int showTable(sqlite3* db){ //Mostrat por pantalla el contenido de la base de da
 
    return 0;
 }
+// -----------------------------------------------------------------------------
 
 
-int main(int argc, char* argv[]) {
-	//Lectura por comandos de IP del servidor para consultas HTTP y ruta y nombre de la base de datos
-	int opt= 0;
-	char *ip_servidor=0,*ruta_bbdd=0;
-	static struct option long_options[] = {
-        {"ip_servidor",	required_argument,	0,	'i'},
-        {"ruta_bbdd",	required_argument,	0,	'r'},
-        {0,								0,	0,	0}
-    };
-    int long_index =0;	
-    while ((opt = getopt_long(argc, argv,"i:r:", 
-                   long_options, &long_index )) != -1) {
-        switch (opt) {
-             case 'i' : ip_servidor = optarg;
-                 break;
-             case 'r' : ruta_bbdd = optarg;
-                 break;
-             default: print_usage(); 
-                 exit(EXIT_FAILURE);
-        }
-    }
-    printf("La IP del servidor seleccionada es: %s\n",ip_servidor);
-    printf("La ruta de la base de datos es: %s\n",ruta_bbdd);
-
-	char date[100];
-	char date_alarm[100];
-	char Alarm_description[100];
-	int ret=0;
-	int value_int;
-	float value_volts;
-	
-	sqlite3 *db;
-	
-	openDB("captura.db", &db);
-	
-	CreateTable(db);
-	
-	CreateTable1(db);
-	
-	CreateTable2(db);
-
-	while(1){
-		ret = spiadc_config_transfer( SINGLE_ENDED_CH2, &value_int );
-
-		//printf("voltatge %.3f V\n", value_volts);
-		//printf("valor llegit (0-1023) %d\n", value_int);
-		//fprintf(stdout, "%lu\n", (unsigned long)t);
-		
-		value_volts=3.3*value_int/1023;
-		time_t t = time(NULL);
-		struct tm tm = *localtime(&t);
-		//printf("Temps actual: %d-%d-%d %d:%d:%d\n", tm.tm_year + 1900, tm.tm_mon + 1,tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);	
-		sprintf(date,"%d-%d-%d %d:%d:%d", tm.tm_mday, tm.tm_mon + 1,tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);	
-
-		insertTable(db,date,value_volts);
-		
-		insertTable1(db,date,value_volts);
-		
-			
-		/*Alarms*/
-		
-			if(value_volts > 2.7){
-			sprintf(Alarm_description,"Exceso de tension");
-			sprintf(date_alarm,"%d-%d-%d %d:%d:%d", tm.tm_mday, tm.tm_mon + 1,tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
-			}
-
-		
-		insertTable2(db,date_alarm,Alarm_description);
-	
-		showTable(db);
-		
-		sleep(5);
-	}
-	
-	sqlite3_close(db);
-	
-	return 0;
-}
